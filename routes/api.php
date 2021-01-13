21c2aa5588fc6db2ade016a79a739252a1810e9d
rachadiannovansyah <rachadian.novansyah15@gmail.com>
2021-01-13 17:54:32 +0700

    resolved conflict

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,15 @@ Route::prefix('v1')->group(function () {
     Route::apiResource('asset', 'AssetController');
     Route::get('asset/list', 'ListController@index');
 });
+
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('/user', 'Settings\ProfileController@index');
+    Route::group(['prefix' => 'reservation', 'namespace' => 'V1'], function () {
+        Route::get('/', 'ReservationController@index');
+        Route::post('/', 'ReservationController@store')->middleware('can:isEmployee');
+        Route::get('/{id}', 'ReservationController@show');
+        Route::get('/booking-list', 'ReservationController@bookingList');
+        Route::put('acceptance/{id}', 'ReservationController@acceptance')->middleware('can:isAdmin');
+        Route::delete('/{id}', 'ReservationController@destroy')->middleware('can:isEmployee');
+    });
+});
