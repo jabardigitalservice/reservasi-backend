5390754a085387a34febea5b5863077a50fbf205
ayocodingit <ayocodingit@gmail.com>
2021-01-14 10:43:10 +0700

    filter role

--- app/Http/Controllers/Settings/ProfileController.php
+++ app/Http/Controllers/Settings/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Settings;
 
+use App\Enums\UserRoleEnum;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -14,16 +15,27 @@ class ProfileController extends Controller
      * @param  mixed $request
      * @return void
      */
+
     public function index(Request $request)
     {
         $token = json_decode(Auth::token());
+
+        $filteredRole = UserRoleEnum::employee_reservasi();
+
+        foreach ($token->realm_access->roles as $role) {
+            if (in_array($role, UserRoleEnum::toArray())) {
+                $filteredRole = $role;
+            }
+        }
+
         $data = [
             'id' => $token->sub,
             'name' => $token->name,
             'username' => $token->preferred_username,
             'email' => $token->email,
-            'realm_access' => $token->realm_access,
+            'role' => $filteredRole,
         ];
+
         return response()->json(['message' => 'success', 'status' => 200, 'data' => $data]);
     }
 }
