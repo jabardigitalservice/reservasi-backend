4b165a40477d05a5b2af6bb865aac8d5e6647f3e
ayocodingit <ayocodingit@gmail.com>
2021-01-14 09:36:32 +0700

    minor config route basic

--- app/Http/Controllers/Settings/ProfileController.php
+++ app/Http/Controllers/Settings/ProfileController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers\Settings;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
+    /**
+     * index
+     *
+     * @param  mixed $request
+     * @return void
+     */
     public function index(Request $request)
     {
-        return $request->user();
+        $token = json_decode(Auth::token());
+        $data = [
+            'id' => $token->sub,
+            'name' => $token->name,
+            'username' => $token->preferred_username,
+            'email' => $token->email,
+            'realm_access' => $token->realm_access,
+        ];
+        return response()->json(['message' => 'success', 'status' => 200, 'data' => $data]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,10 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
+Route::get('/', 'HomeController');
+
 Route::group(['middleware' => ['auth:api']], function () {
-    Route::get('user', 'HomeController');
+    Route::get('user', 'Settings/ProfileController@index');
     Route::apiResource('asset', 'AssetController');
     Route::get('asset/list', 'ListController@index');
     Route::group(['namespace' => 'V1'], function () {
