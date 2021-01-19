9340d563c70bb93ddbd20eb43dbc603bef9a778d
Rindi Budiaramdhan <budiaramdhanrindi@gmail.com>
2021-01-19 16:28:39 +0700

    Bug Fix Code Statistik Reservasi

--- app/Http/Controllers/V1/DashboardController.php
+++ app/Http/Controllers/V1/DashboardController.php
@@ -19,18 +19,24 @@ class DashboardController extends Controller
      */
     public function reservationStatistic(Request $request)
     {
-        $reservation = Reservation::query();
-
+        $all = Reservation::query();
+        $notYetApproved = Reservation::where('approval_status', ReservationStatusEnum::not_yet_approved());
+        $alreadyApproved = Reservation::where('approval_status', ReservationStatusEnum::already_approved());
+        $rejected = Reservation::where('approval_status', ReservationStatusEnum::rejected());
+        
         //check role employee reservasi
         if (User::getUser()->role == UserRoleEnum::employee_reservasi()) {
-            $reservation->where('user_id_reservation', User::getUser()->id);
+            $all->where('user_id_reservation', User::getUser()->id);
+            $notYetApproved->where('user_id_reservation', User::getUser()->id);
+            $alreadyApproved->where('user_id_reservation', User::getUser()->id);
+            $rejected->where('user_id_reservation', User::getUser()->id);
         }
-
+        
         $statistic = [
-            'all' => $reservation->count(),
-            'not_yet_approved' => $reservation->where('approval_status', ReservationStatusEnum::not_yet_approved())->count(),
-            'already_approved' => $reservation->where('approval_status', ReservationStatusEnum::already_approved())->count(),
-            'rejected' => $reservation->where('approval_status', ReservationStatusEnum::rejected())->count(),
+            'all' => $all->count(),
+            'not_yet_approved' => $notYetApproved->count(),
+            'already_approved' => $alreadyApproved->count(),
+            'rejected' => $rejected->count(),
         ];
 
         return $statistic;
