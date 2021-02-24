9051d4a46524b77109c508255ac98d4cd46940fc
Yonathan Setiadi <setiadi_y56@yahoo.co.id>
2021-02-24 08:50:45 +0700

    #42 add drop Column for join url

--- database/migrations/2021_02_21_224312_add_join_url_to_reservations_table.php
+++ database/migrations/2021_02_21_224312_add_join_url_to_reservations_table.php
@@ -26,7 +26,7 @@ class AddJoinUrlToReservationsTable extends Migration
     public function down()
     {
         Schema::table('reservations', function (Blueprint $table) {
-            //
+            $table->dropColumn('join_url');
         });
     }
 }
