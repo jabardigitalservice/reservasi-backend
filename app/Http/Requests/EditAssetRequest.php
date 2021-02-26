a6935b1cb1b522d5786f712eb37d96702b87cf44
Muhamad Rizky Rahmansyah <muhamad.rizky1996@gmail.com>
2021-02-26 09:16:25 +0700

    #62 fix cant edit capacity

--- app/Http/Requests/EditAssetRequest.php
+++ app/Http/Requests/EditAssetRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use App\Enums\AssetStatusEnum;
+use App\Enums\ResourceTypeEnum;
+use Spatie\Enum\Laravel\Rules\EnumValueRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Spatie\Enum\Laravel\Rules\EnumRule;
 
@@ -28,6 +30,8 @@ class EditAssetRequest extends FormRequest
         return [
             'name' => 'required|unique:assets,name,' . $this->asset->id . ',id,deleted_at,NULL',
             'status' => ['required', new EnumRule(AssetStatusEnum::class)],
+            'capacity' => 'required|numeric',
+            'resource_type' => ['required', new EnumValueRule(ResourceTypeEnum::class)],
         ];
     }
 }
