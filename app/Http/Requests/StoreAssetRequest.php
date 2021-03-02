dd86c8169ea66594a209b4c0eed55297fc540ece
Novansyah <rachadian.novansyah15@gmail.com>
2021-03-02 18:56:29 +0700

    fixing description always null when store an asset

--- app/Http/Requests/StoreAssetRequest.php
+++ app/Http/Requests/StoreAssetRequest.php
@@ -30,7 +30,8 @@ class StoreAssetRequest extends FormRequest
             'name' => 'required|unique:assets,name,NULL,id,deleted_at,NULL',
             'status' => ['required', new EnumValueRule(AssetStatusEnum::class)],
             'capacity' => 'required|numeric',
-            'resource_type' => ['required', new EnumValueRule(ResourceTypeEnum::class)]
+            'resource_type' => ['required', new EnumValueRule(ResourceTypeEnum::class)],
+            'description' => 'nullable'
         ];
     }
 }
