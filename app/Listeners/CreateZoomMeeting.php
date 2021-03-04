9a3ea4f7f3e836e7a727c5eb0596e085a17a62e7
Yonathan Setiadi <setiadi_y56@yahoo.co.id>
2021-03-04 10:27:00 +0700

    #69 add host email of zoom

--- app/Listeners/CreateZoomMeeting.php
+++ app/Listeners/CreateZoomMeeting.php
@@ -4,7 +4,7 @@ namespace App\Listeners;
 
 use App\Events\AfterReservation;
 use App\Enums\ResourceTypeEnum;
-use MacsiDigital\Zoom\Facades\Zoom;
+use \MacsiDigital\Zoom\Facades\Zoom;
 
 class CreateZoomMeeting
 {
