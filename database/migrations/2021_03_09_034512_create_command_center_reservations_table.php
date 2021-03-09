c7c084c5bdbf6380af9401dc08824e6ada85d9fe
Indra Prasetya <indraprasetya154@gmail.com>
2021-03-09 12:32:38 +0700

    Change field date name and add index

--- database/migrations/2021_03_09_034512_create_command_center_reservations_table.php
+++ database/migrations/2021_03_09_034512_create_command_center_reservations_table.php
@@ -16,16 +16,16 @@ class CreateCommandCenterReservationsTable extends Migration
         Schema::create('command_center_reservations', function (Blueprint $table) {
             $table->id();
             $table->uuid('user_id_reservation');
-            $table->string('reservation_code');
-            $table->string('name');
+            $table->string('reservation_code')->index();
+            $table->string('name')->index();
             $table->integer('nik')->unique();
             $table->string('organization')->nullable();
             $table->string('organization_address')->nullable();
-            $table->string('phone_number');
-            $table->integer('email')->unique();
+            $table->string('phone_number')->index();
+            $table->integer('email')->unique()->index();
             $table->string('purpose');
             $table->integer('participant');
-            $table->dateTime('date');
+            $table->dateTime('reservation_date')->index();
             $table->enum('shift', ['shift_1', 'shift_2']);
             $table->enum('approval_status', ['not_yet_approved', 'already_approved', 'rejected'])
                 ->default('not_yet_approved');
@@ -34,7 +34,6 @@ class CreateCommandCenterReservationsTable extends Migration
             $table->uuid('user_id_updated')->nullable();
             $table->uuid('user_id_deleted')->nullable();
             $table->softDeletes();
-            $table->index(['nik', 'reservation_code', 'phone_number' , 'email', 'date']);
             $table->timestamps();
         });
     }
